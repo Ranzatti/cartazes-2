edbb689df1be9bc8a1176936b986ef135bb1c395
Ricardo Ranzatti <ranzatti@sonner.com.br>
2024-11-08 16:10:06 -0300

    ajustes

--- app.py
+++ app.py
@@ -128,6 +128,37 @@ if modal.is_open():
                                 st.write("") 
                     st.divider()
 
+                    # pegando o elento
+                    resposta = rq.get(f'https://api.themoviedb.org/3/movie/{tmdb}/credits?api_key=2b0120b7e901bbe70b631b2273fe28c9')
+                    if resposta.status_code == 200:
+                            imagens = resposta.json()
+                            cast = imagens['cast']
+                            
+                            # Exibindo os elenco horizontalmente com colunas
+                            st.subheader("Elenco Principal")
+
+                            # Defina um tamanho maior para as colunas
+                            num_colunas = 8  # Defina quantas colunas você quer por linha
+                            cols = st.columns(num_colunas)
+
+                            # carrego todos os posters não nulos num vetor
+                            atores_com_imagem = [ator for ator in cast if ator.get('profile_path')]
+
+                            # varro o vetor e mostro as imagens
+                            for i, ator in enumerate(atores_com_imagem):
+                                img_url = f"https://image.tmdb.org/t/p/w600_and_h900_bestv2{ator['profile_path']}"
+                                
+                                # Usa um espaçamento e ajusta a largura da imagem
+                                with cols[i % num_colunas]:  # Adiciona a imagem na coluna correspondente
+                                    st.image(img_url, width=90)  # Ajusta a largura da imagem
+                                    #st.write(ator['name'])  # Exibe o nome do ator abaixo da imagem
+                                    st.markdown(f"<p style='font-size:10px;  text-align: center;'>{ator['name']}</p>", unsafe_allow_html=True)
+
+                            # Se você quiser adicionar uma nova linha após cada conjunto de imagens
+                            if len(cast) > num_colunas:
+                                st.write("") 
+                    st.divider()
+
 
                     # ate aqui
 
